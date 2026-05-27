f7766a43e5c2ba5034175ca5bfff27ce66646995
Rodrigo Ignacio <ron.pavezb@gmail.com>
2026-05-26 20:13:21 -0400

    Initial Commit

---
+++ BookStoreSwiftUIApp/BookStoreSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreSwiftUIAppApp.swift
+//  BookStoreSwiftUIApp
+//
+//  Created by Rodrigo Ignacio on 26-05-26.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
